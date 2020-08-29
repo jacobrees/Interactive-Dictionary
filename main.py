59d00af989df3064966c9fc0ba4a5ca106c9f540
jacobrees <jacob.rees97@yahoo.com>
2020-08-29 12:20:26 +0100

    Optimize output to print each answer in array seperately

--- main.py
+++ main.py
@@ -20,4 +20,9 @@ def translate(word):
 
 word = input("Enter Word: ")
 
-print(translate(word))
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
