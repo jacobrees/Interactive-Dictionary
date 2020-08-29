feccaa6bf17db729af7dcb1018db6b6ef49ad955
jacobrees <jacob.rees97@yahoo.com>
2020-08-29 12:03:35 +0100

    Add exception handling to program incase user enters a word that doesn't exist in the english language

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import json
 data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return "This word doesn't exist in the english language..."
 
 word = input("Enter Word: ")
 
